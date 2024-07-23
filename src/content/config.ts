066770f3c78033c1b484de2bd106574b735662b8
jrolana <olanajhoanna@gmail.com>
2024-07-23 19:41:02 +0800

    [Fix][Olana] Work to Projects Routing

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-	work: defineCollection({
+	projects: defineCollection({
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
