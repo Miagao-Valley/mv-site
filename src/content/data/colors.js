0083ba53e84362603f81090ee5d9d5539bbf461f
jrolana <olanajhoanna@gmail.com>
2024-07-26 17:28:31 +0800

    [Adjustment][Olana] Langauges Used Section

---
+++ src/content/data/colors.js
@@ -0,0 +1,19 @@
+export { colors };
+
+const url = "https://www.csscolorsapi.com/api/colors/group/purple";
+
+async function getColors(url) {
+    const response = await fetch(url);
+    const results = await response.json();
+    const colors = await results.colors;
+
+    const colorCodes = []
+    colors.forEach((color) => {
+        colorCodes.push(`#${color.hex}`);
+    });
+
+    return colorCodes;
+}
+
+const colors = await getColors(url);
+console.log(colors);
